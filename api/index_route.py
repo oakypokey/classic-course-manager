2325d0f5d8b454ea60c725dc1467c5f4a4c0cbe9
Calvin <cod11@georgetown.edu>
2020-04-08 15:43:46 -0400

    one more time

--- api/index_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, flash, redirect, request
-
-index_route = Blueprint("index", __name__)
-
-@index_route.route("/")
-def index():
-    return index_route.send_static_file("index.html")
