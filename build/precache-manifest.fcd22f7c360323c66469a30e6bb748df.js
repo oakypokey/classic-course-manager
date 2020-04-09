e3528e313583756fe459e7a5557eafbfd9c4c721
Calvin <cod11@georgetown.edu>
2020-04-09 16:18:26 -0400

    update

---
+++ build/precache-manifest.fcd22f7c360323c66469a30e6bb748df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e85e93cdfebccc0dea59a95d08b5291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922359183996b91328c7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4e763dcd2ce79dbff5b1",
+    "url": "/static/css/main.bf15dac7.chunk.css"
+  },
+  {
+    "revision": "922359183996b91328c7",
+    "url": "/static/js/2.b2b7ca02.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.b2b7ca02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e763dcd2ce79dbff5b1",
+    "url": "/static/js/main.1d42fe4a.chunk.js"
+  },
+  {
+    "revision": "61b9f68396e2d483955d",
+    "url": "/static/js/runtime-main.8cb5001c.js"
+  }
+]);
